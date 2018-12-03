c4017911337ec45562dec77ef987038d9c3615a9
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-12-03 14:56:44 +0530

    Usage of head

---
+++ head.js
@@ -0,0 +1,18 @@
+/* 
+  Usage:
+  node ./head.js file1
+  node ./head.js -n5 file1
+  node ./head.js -n 5 file1
+  node ./head.js -5 file1
+  node ./head.js file1 file2
+  node ./head.js -n 5 file1 file2
+  node ./head.js -n5 file1 file2
+  node ./head.js -5 file1 file2 
+  node ./head.js -c5 file1
+  node ./head.js -c 5 file1
+  node ./head.js -c5 file1 file2
+  node ./head.js -c 5 file1 file2
+*/
+
+
+
