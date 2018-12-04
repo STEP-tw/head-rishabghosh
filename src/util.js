24f07f742f89c53d145e0a2d387733e44c789634
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-04 19:05:48 +0530

    readFile function is modified, documentation removed
    previously readFile was removing the last elements of output array
    by slicing it, that featurn was removed to fix bugs

--- src/util.js
+++ src/util.js
@@ -1,9 +1,5 @@
-/*
- * fs.readFileSync should be passed as 2nd arg in readFile function
- */
-
 const readFile = function(filename, reader) {
-  let contentOfFile = reader("./"+filename, "utf8").split("\n");
+  let contentOfFile = reader(filename, "utf8").split("\n");
   return contentOfFile;
 };
 
