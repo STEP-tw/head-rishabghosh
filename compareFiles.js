cdf013e017b4b1015e764042c1184fb2a5d1fd70
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-06 10:50:01 +0530

    compareFiles trims each file before comparing
    any space, new line is trimmed before the contents of files are stored
    in variavble for comparison.
    
    the trim() function is used to trim the files

--- compareFiles.js
+++ compareFiles.js
@@ -4,8 +4,8 @@ const green = "\x1b[32m";
 const red = "\x1b[31m";
 const reset = "\x1b[0m";
 
-let myHead = fs.readFileSync("./.myHead", "utf8");
-let sysHead = fs.readFileSync("./.sysHead", "utf8");
+let myHead = fs.readFileSync("./.myHead", "utf8").trim();
+let sysHead = fs.readFileSync("./.sysHead", "utf8").trim();
 let message = red + "Not Matched" + reset;
 
 if (myHead == sysHead) {
