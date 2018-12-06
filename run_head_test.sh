6218d5ef1f6d8b0ee2b03ba40334a43dad44b86a
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-06 10:52:24 +0530

    More test cases added in run_head_test.sh

--- run_head_test.sh
+++ run_head_test.sh
@@ -5,6 +5,7 @@ echo "plz provide sample file, ignore if already provided \n"
 LIGHTGRAY='\033[0;37m'
 NC='\033[0m'
 input_file=$1
+input_file2=$2
 
 head -n5 $input_file > .sysHead
 node head.js -n5 $input_file > .myHead
@@ -21,29 +22,24 @@ node head.js -5 $input_file > .myHead
 echo "for format -> node head.js -5 file1"
 node compareFiles.js
 
-head $input_file $input_file > .sysHead
-node head.js $input_file $input_file > .myHead
+head $input_file $input_file2 > .sysHead
+node head.js $input_file $input_file2 > .myHead
 echo "for format -> node head.js file1 file2"
 node compareFiles.js
 
-head -n 5 $input_file $input_file > .sysHead
-node head.js -n 5 $input_file $input_file > .myHead
+head -n 5 $input_file $input_file2 > .sysHead
+node head.js -n 5 $input_file $input_file2 > .myHead
 echo "for format -> node head.js -n 5 file1 file2"
 node compareFiles.js
 
 
-head -n5 $input_file $input_file > .sysHead
-node head.js -n5 $input_file $input_file > .myHead
+head -n5 $input_file $input_file2 > .sysHead
+node head.js -n5 $input_file $input_file2 > .myHead
 echo "for format -> node head.js -n5 file1 file2"
 node compareFiles.js
 
-head -n15 $input_file $input_file > .sysHead
-node head.js -n15 $input_file $input_file > .myHead
-echo "for format -> node head.js -n15 file1 file2"
-node compareFiles.js
-
-head -5 $input_file $input_file > .sysHead
-node head.js -5 $input_file $input_file > .myHead
+head -5 $input_file $input_file2 > .sysHead
+node head.js -5 $input_file $input_file2 > .myHead
 echo "for format -> node head.js -5 file1 file2"
 node compareFiles.js
 
@@ -57,15 +53,26 @@ node head.js -c 5 $input_file > .myHead
 echo "for format -> node head.js -c 5 file1"
 node compareFiles.js
 
-head -c5 $input_file $input_file > .sysHead
-node head.js -c5 $input_file $input_file > .myHead
+head -c5 $input_file $input_file2 > .sysHead
+node head.js -c5 $input_file $input_file2 > .myHead
 echo "for format -> node head.js -c5 file1 file2"
 node compareFiles.js
 
-head -c 5 $input_file $input_file > .sysHead
-node head.js -c 5 $input_file $input_file > .myHead
+head -c 5 $input_file $input_file2 > .sysHead
+node head.js -c 5 $input_file $input_file2 > .myHead
 echo "for format -> node head.js -c 5 file1 file2"
 node compareFiles.js
+
+head -n11 $input_file > .sysHead
+node head.js -n11 $input_file > .myHead
+echo "for format -> node head.js -n11 file1 file2"
+node compareFiles.js
+
+head -n16 $input_file > .sysHead
+node head.js -n16 $input_file > .myHead
+echo "for format -> node head.js -n16 file1 file2"
+node compareFiles.js
+
 
 #head -- 5 $input_file > .sysHead
 #node head.js -- 5 $input_file 2> .myHead
