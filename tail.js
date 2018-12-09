a9f7a18505d8c5fd6117d4028d8ad7f9b958d7a7
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-12-09 23:59:03 +0530

    new assignment

---
+++ tail.js
@@ -0,0 +1,18 @@
+/* 
+  Usage:
+  node ./tail.js file1
+  node ./tail.js -n5 file1
+  node ./tail.js -n 5 file1
+  node ./tail.js -5 file1
+  node ./tail.js file1 file2
+  node ./tail.js -n 5 file1 file2
+  node ./tail.js -n5 file1 file2
+  node ./tail.js -5 file1 file2 
+  node ./tail.js -c5 file1
+  node ./tail.js -c 5 file1
+  node ./tail.js -c5 file1 file2
+  node ./tail.js -c 5 file1 file2
+*/
+
+
+
