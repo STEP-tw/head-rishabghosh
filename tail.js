9bcde35aa38b01a25fa39ac1f2f7aa5acba4e558
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-10 19:41:30 +0530

    Implimented main in tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,14 @@
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
+/*eslint-env node*/
+
+const fs = require("fs");
+
+const { tail } = require("./src/lib.js");
+
+const main = function(userInput) {
+  return tail(userInput, fs);
+};
+
+console.log(main(process.argv));
 
 
 
