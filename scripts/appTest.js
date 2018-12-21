978af898026b11426f78e710668429f6c0513e5c
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-21 19:07:28 +0530

    Modified appTest.js
    its ready take test data from a file

--- scripts/appTest.js
+++ scripts/appTest.js
@@ -1,4 +1,7 @@
-const shellCmd = require("child_process").execSync;
+/*eslint-env node*/
+const shelljs = require("shelljs");
+const { testData } = require('./appTestData.js');
+
 const file1 = process.argv[3];
 const file2 = process.argv[4];
 const utility = process.argv[2];
@@ -6,14 +9,19 @@ const green = "\x1b[32m";
 const red = "\x1b[31m";
 const reset = "\x1b[0m";
 
+const shellCmd = function(commandLineArg) {
+  return shelljs.exec(commandLineArg, {silent:true});
+};
+
 const executeShellCmd = function(utility, args) {
-  let totalArgs = args.reduce( (acc, value)=> acc + " " + value, utility);
-  const shellOut = shellCmd(totalArgs).toString("utf8");
+  const totalArgs = args.reduce( (acc, value)=> acc + " " + value, utility);
+  const shellOut = shellCmd(totalArgs).output;
   return { shellOut, totalArgs };
 };
 
 const createSysOut = function(...options) {
   let userArgs = options.slice();
+  //add a if to push two files
   userArgs.push(file1);
   const { shellOut, totalArgs } = executeShellCmd(utility, userArgs);
   return { shellOut, totalArgs };
@@ -33,7 +41,6 @@ const logStatus = function(){
   const method = "for " + totalArgs ;
   const sysOut = createSysOut("").shellOut.trim();
   const userOut = createUserOut("").shellOut.trim();
-  //let method = "for "
   if(sysOut === userOut) {
     message = green + "Match Found" + reset;
   }
